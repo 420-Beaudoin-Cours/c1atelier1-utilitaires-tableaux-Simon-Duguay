a0fa4eeb9c76711c050677291e54ee06c0a0052f
Simon-Duguay <simon.duguay2000@gmail.com>
2020-01-24 10:55:32 -0500

    Terminé

--- Tableaux.java
+++ Tableaux.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
@@ -56,71 +55,110 @@ public class Tableaux {
             if (imax != i)
                 permute(tab, i, imax);
         }
-        print(tab);
+    }
+
+    public static void trierDecroissantSSS(String[] tab) {
+        int imax;
+        for (int i = 0; i < tab.length - 1; i++) {
+            imax = i;
+            for (int j = i + 1; j < tab.length; j++) {
+                if (Integer.parseInt(tab[j]) > Integer.parseInt(tab[imax]))
+                    imax = j;
+            }
+            if (imax != i)
+                permute(tab, i, imax);
+        }
     }
 
     public static void print(int[] tab) {
         System.out.print(Arrays.toString(tab));
     }
+
     public static void print(String[] tab) {
         System.out.print(Arrays.toString(tab));
     }
 
-    public static void maximum(int[] tab) {
+    public static int maximum(int[] tab) {
         int max = tab[0];
 
         for (int nb : tab) {
             if (nb > max)
                 max = nb;
         }
-
+        return max;
     }
 
-    public static void minimum(int[] tab) {
+    public static int maximum(String[] tab) {
+        int max = Integer.parseInt(tab[0]);
+        int nb;
+
+        for (String strNb : tab) {
+            nb = Integer.parseInt(strNb);
+            if (nb > max)
+                max = nb;
+        }
+        return max;
+    }
+
+    public static int minimum(int[] tab) {
         int min = tab[0];
 
-        for (int nb: tab) {
+        for (int nb : tab) {
             if (nb < min)
                 min = nb;
         }
-
+        return min;
     }
-    public static void moyenne(int[] tab){
+
+    public static int minimum(String[] tab) {
+        int min = Integer.parseInt(tab[0]);
+        int nb;
+
+        for (String strNb : tab) {
+            nb = Integer.parseInt(strNb);
+            if (nb < min)
+                min = nb;
+        }
+        return min;
+    }
+
+    public static int moyenne(int[] tab) {
         int somme = 0;
         int moyenne;
 
-        for (int nb: tab) {
+        for (int nb : tab) {
             somme += nb;
         }
-        moyenne = somme/tab.length;
+        moyenne = somme / tab.length;
+        return moyenne;
 
     }
-    public static int fouilleSeq(int[] tab, int elem){
-        for (int index = 0; index < tab.length ; index++) {
+
+    public static int moyenne(String[] tab) {
+        int somme = 0;
+        int moyenne;
+
+        for (String strnb : tab) {
+            somme += Integer.parseInt(strnb);
+        }
+        moyenne = somme / tab.length;
+        return moyenne;
+    }
+
+    public static int fouilleSeq(int[] tab, int elem) {
+        for (int index = 0; index < tab.length; index++) {
             if (tab[index] == elem)
                 return index;
         }
         return -1;
     }
-    public static int fouilleSeq(String[] tab, String elem){
-        for (int index = 0; index < tab.length ; index++) {
+
+    public static int fouilleSeq(String[] tab, String elem) {
+        for (int index = 0; index < tab.length; index++) {
             if (tab[index].equals(elem))
                 return index;
         }
         return -1;
     }
-    public static void trierDecroissantSSS(String[] tab){
-        int imax;
-        for (int i = 0; i < tab.length - 1 ; i++) {
-            imax = i;
-            for (int j = i+1; j < tab.length ; j++) {
-                if (Integer.parseInt(tab[j]) > Integer.parseInt(tab[imax]))
-                    imax = j;
-            }
-            if (imax != i)
-                permute(tab,i,imax);
-        }
-        print(tab);
-    }
-}
 
+}
